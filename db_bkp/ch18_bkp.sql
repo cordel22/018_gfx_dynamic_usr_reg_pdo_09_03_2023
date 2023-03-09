c13e8a9437fbf92be37a5b5fa842c6adf8d19ed1
cordel22 <cordelfenevall@gmail.com>
2023-03-09 14:50:19 +0100

    initial commit 09032023

---
+++ db_bkp/ch18_bkp.sql
@@ -0,0 +1,42 @@
+-- --------------------------------------------------------
+-- Hostiteľ:                     127.0.0.1
+-- Verze serveru:                10.6.4-MariaDB - mariadb.org binary distribution
+-- OS serveru:                   Win32
+-- HeidiSQL Verzia:              11.3.0.6295
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Exportování struktury databáze pro
+CREATE DATABASE IF NOT EXISTS `ch18` /*!40100 DEFAULT CHARACTER SET utf8mb3 */;
+USE `ch18`;
+
+-- Exportování struktury pro tabulka ch18.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(20) NOT NULL,
+  `last_name` varchar(40) NOT NULL,
+  `email` varchar(60) NOT NULL,
+  `pass` char(40) NOT NULL,
+  `user_level` tinyint(1) unsigned NOT NULL DEFAULT 0,
+  `active` char(32) DEFAULT NULL,
+  `registration_date` datetime NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `email` (`email`),
+  KEY `login` (`email`,`pass`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- Exportování dat pro tabulku ch18.users: ~0 rows (přibližně)
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
---
+++ readme.txt
@@ -0,0 +1,17 @@
+
+
+header
+
+
+
+config.inc.php
+
+BASE_URL updateni
+MYSQL	updateni
+
+LIVE na true v tom error handlngu
+
+mysqli_connect.
+
+
+smtp
